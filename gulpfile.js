2f608a0cd4d0d9fe8bf683f00534adb2d5f17ecf
d-h-e <dirkhenn+github@gmail.com>
2019-02-22 19:37:45 +0100

    added mediaqueries combining and sorting

--- gulpfile.js
+++ gulpfile.js
@@ -6,39 +6,65 @@ const uglify = require('gulp-uglify-es').default;
 const rename = require('gulp-rename');
 const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
+const cssmqpacker = require('css-mqpacker'); // prevent media query bubbling
+
+const mqoptions = {
+  sort: function(a, b) {
+      const regex = /(\d+)/g;
+      const regex2 = /(\d+)/g;
+      var aa = regex.exec(a);
+      var bb = regex2.exec(b);
+      var an = null;
+      var bn = null;
+      if (aa != null) {
+          an = parseInt(aa);
+      }
+      if (bb != null) {
+          bn = parseInt(bb);
+      }
+      if (a.indexOf('max-width') != -1 && b.indexOf('max-width') != -1) {
+          if (an < bn) return 1;
+          if (an > bn) return -1;
+          return 0;
+      }
+      return a.localeCompare(b);
+  }
+}
 
 const css = (cb) => {
-  src('./scss/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([autoprefixer()]))
-    .pipe(sourcemaps.write('.'))
-    .pipe(dest('./css'))
-    .pipe(browserSync.stream());
+    src('./scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([autoprefixer(), cssmqpacker(mqoptions)]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./css'))
+        .pipe(browserSync.stream());
 
-  cb();
+    cb();
 };
 
 const js = (cb) => {
-  src('./index.js')
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(dest('./'))
-    .pipe(browserSync.stream());
+    src('./index.js')
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(dest('./'))
+        .pipe(browserSync.stream());
 
-  cb();
+    cb();
 };
 
 const watcher = () => {
-  browserSync.init({
-    server: {
-      basedir: './'
-    }
-  });
+    browserSync.init({
+        server: {
+            basedir: './'
+        }
+    });
 
-  watch('./scss/**/*.scss', css);
-  watch('./index.js', js);
-  watch('./*.html').on('change', browserSync.reload);
+    watch('./scss/**/*.scss', css);
+    watch('./index.js', js);
+    watch('./*.html').on('change', browserSync.reload);
 };
 
 exports.css = css;
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const test = 'bla';
 console.log('js init');
+console.log('js init');
 
 const myFn = () => {
   console.log(test);
 };
 
 myFn();
+
+const mySort =  (a, b) => {
+  return a.localeCompare(b);
+}
+
