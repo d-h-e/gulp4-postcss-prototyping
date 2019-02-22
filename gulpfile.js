6c31271f8cc497ff269ec6db9e80eb5601d37155
d-h-e <dhe@internetschmie.de>
2019-02-22 11:38:19 +0100

    added index.js watch to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
 
@@ -17,6 +19,16 @@ const css = (cb) => {
   cb();
 };
 
+const js = (cb) => {
+  src('./index.js')
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest('./'))
+    .pipe(browserSync.stream());
+
+  cb();
+};
+
 const watcher = (cb) => {
   browserSync.init({
     server: {
@@ -25,8 +37,8 @@ const watcher = (cb) => {
   });
 
   watch('./scss/**/*.scss', css);
+  watch('./index.js', js);
   watch('./*.html').on('change', browserSync.reload);
-  watch('./**/*.js').on('change', browserSync.reload);
 
   cb();
 };
