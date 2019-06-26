a02784754b86329d730511ecef5099de8bec0221
d-h-e <dhe@internetschmie.de>
2019-06-26 09:03:44 +0200

    changed to eslint 6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,22 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-    extends: [
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    ],
-    plugins: ['@typescript-eslint'],
-    parserOptions: {
-        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
-    },
-    rules: {
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-        "no-debugger": "warn",
-        "no-console": "warn",
-        "prefer-const": "error",
-        "no-unused-vars": [
-            1,
-            {
-                "argsIgnorePattern": "res|next|^err"
-            }
-        ],
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "brace-style": [
-            "error",
-            "1tbs"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "@typescript-eslint/no-var-requires" : "off"
-    },
     "env": {
         "browser": true,
-        "node": true,
-        "mongo": true,
-        "es6": true,
-        "jquery": true,
-        "jest": true
+        "es6": true
+    },
+    "extends": "standard",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module",
+        "experimentalObjectRestSpread": true,
+        "impliedStrict": true,
+        "classes": true,
+        "legacyDecorators": true
+    },
+    "rules": {
+        "semi": [2, "always"]
     }
 };
